a21abac1fd095519e7220b03df34bb5ea1838ec4
fares mohalhel khalil <fares.mohalhel@gmail.com>
2023-03-19 15:24:50 +0300

    added loops task

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const userName = prompt("Enter your name!");
-const userGender = prompt("Enter your gender!");
+let userGender = prompt("Enter your gender!").toLowerCase();
+let userInfo = [];
+
+while (userGender !== "male" && userGender !== "female") {
+  userGender = prompt("Please Enter your gender correctly.").toLowerCase();
+}
+
 const welcomeMessage = "Welcome to Cafe Z";
 
-if (userGender === "Male" || userGender === "male") {
-  alert(welcomeMessage + " Mr " + userName);
-} else if (userGender === "Female" || userGender === "female") {
-  alert(welcomeMessage + " Ms " + userGender);
+if (userGender === "male") {
+  alert(welcomeMessage + ",Mr " + userName);
+} else if (userGender === "female") {
+  alert(welcomeMessage + ",Ms " + userGender);
 } else {
   alert(welcomeMessage);
 }
@@ -13,6 +19,15 @@ if (userGender === "Male" || userGender === "male") {
 const userDrinkType = prompt("Would you like a hot or a cold drink?");
 const userDrinkName = prompt("Enter drink name!");
 
+userInfo[0] = userName;
+userInfo[1] = userGender;
+userInfo[2] = userDrinkType;
+userInfo[3] = userDrinkName;
+
+for (let i = 0; i < userInfo.length; i++) {
+  console.log(userInfo[i]);
+}
+
 alert("Your drink is being prepared!");
 
-console.log(`username = ${userName}, drink = ${userDrinkName}`);
+// console.log(`username = ${userName}, drink = ${userDrinkName}`);
